7c56501d9f992cb5dff23a1d966de58ceb2795ed
Mitnez <68821845+Mitnez@users.noreply.github.com>
2021-04-13 19:56:31 -0500

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,70 @@
+# Codigo por m1t (basdo en noob-hackers)
+
+# modulos requeridos
+
+import argsparse
+import request, json
+import sys
+from sys import argv
+import os
+
+# ni puta idea, mitnez del futuro sabe que pedo
+ parser = argparse.ArgumentParser()
+ 
+ parser.add_argument ("-ip", help="direccion ip", type =str, dest='target', required=true)
+ 
+ args = parser.parse_args()
+ 
+ # banner que el mit se diseño 
+ print("                   ▄              ▄
+                  ▌▒█           ▄▀▒▌
+                  ▌▒▒█        ▄▀▒▒▒▐
+                 ▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐
+               ▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐
+             ▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌
+            ▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌
+            ▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐
+           ▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌
+           ▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌
+          ▌▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐
+          ▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌
+          ▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐
+           ▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌
+           ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐
+            ▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌
+              ▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀
+                ▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀          Hecho por m1t ")
+                
+                ip= args.target
+                api = "http://ip-api.com/json/"
+                try
+                data = requests.get(api+ip).json()
+                sys.stdout.flush()
+                rint (a, "[Victim]:", data['query'])
+                        print(red+"<--------------->"+red)
+                        print (b, "[ISP]:", data['isp'])
+                        print(red+"<--------------->"+red)
+                        print (a, "[Organisation]:", data['org'])
+                        print(red+"<--------------->"+red)
+                        print (b, "[City]:", data['city'])
+                        print(red+"<--------------->"+red)
+                        print (a, "[Region]:", data['region'])
+                        print(red+"<--------------->"+red)
+                        print (b, "[Longitude]:", data['lon'])
+                        print(red+"<--------------->"+red)
+                        print (a, "[Latitude]:", data['lat'])
+                        print(red+"<--------------->"+red)
+                        print (b, "[Time zone]:", data['timezone'])
+                        print(red+"<--------------->"+red)
+                        print (a, "[Zip code]:", data['zip'])
+                        print (" "+yellow)
+                        
+                       except KeyboardInterrupt:
+                        print ("Acabando programa adios")
+                        sys.exit(0)
+                       except requests.exceptions.ConnectionError as e:
+                        print ("Checa la conexion a internet porque si no no sirvo")
+                       sys.exit(1)                        
+                
+                
+                
