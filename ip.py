c4295ca4457ea3705d24baa7049b27389a3cc955
Mitnez <68821845+Mitnez@users.noreply.github.com>
2021-04-15 12:30:19 -0500

    Add files via upload

--- ip.py
+++ ip.py
@@ -17,9 +17,9 @@ args = parser.parse_args()
 
 #banner of script
 
-print (lgreen+bold+"         <===[[ coded by N17RO ]]===> \n"+clear)
-print (yellow+bold+"   <---(( search on youtube Noob Hackers ))--> \n"+clear)
-print (yellow+bold+"   <---(( Traducido por m1t xdxd ))--> \n"+clear)
+print ("         <===[[ coded by N17RO ]]===> \n")
+print ("   <---(( search on youtube Noob Hackers ))--> \n"+clear)
+print ("   <---(( Traducido por m1t xdxd ))--> \n")
 
 
 ip = args.target
@@ -29,30 +29,27 @@ api = "http://ip-api.com/json/"
 try:
         data = requests.get(api+ip).json()
         sys.stdout.flush()
-        a = lgreen+bold+"[$]"
-        b = cyan+bold+"[$]"
-        print (a, "[Victima]:", data['query'])
-        print(red+"<--------------->"+red)
-        print (b, "[Proveedor]:", data['isp'])
-        print(red+"<--------------->"+red)
-        print (a, "[Organizacion]:", data['org'])
-        print(red+"<--------------->"+red)
-        print (b, "[Ciudad]:", data['city'])
-        print(red+"<--------------->"+red)
-        print (a, "[Region]:", data['region'])
-        print(red+"<--------------->"+red)
-        print (b, "[Longitud]:", data['lon'])
-        print(red+"<--------------->"+red)
-        print (a, "[Latitiud]:", data['lat'])
-        print(red+"<--------------->"+red)
-        print (b, "[Zona horaria]:", data['timezone'])
-        print(red+"<--------------->"+red)
+        print ("[Victima]:", data['query'])
+        print("<--------------->")
+        print ("[Proveedor]:", data['isp'])
+        print("<--------------->")
+        print ("[Organizacion]:", data['org'])
+        print("<--------------->")
+        print ("[Ciudad]:", data['city'])
+        print("<--------------->")
+        print ("[Region]:", data['region'])
+        print("<--------------->")
+        print ("[Longitud]:", data['lon'])
+        print("<--------------->")
+        print ("[Latitiud]:", data['lat'])
+        print("<--------------->")
+        print ("[Zona horaria]:", data['timezone'])
+        print("<--------------->")
         print (a, "[Codigo postal]:", data['zip'])
-        print (" "+yellow)
 
 except KeyboardInterrupt:
-        print ('Adios, ten un buen dia :)'+lgreen)
+        print ('Adios, ten un buen dia :)')
         sys.exit(0)
 except requests.exceptions.ConnectionError as e:
-        print (red+"[~]"+" No tengo internet, no sirvo"+clear)
+        print ("[~]"+" No tengo internet, no sirvo")
 sys.exit(1)
