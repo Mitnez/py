b05cdf31c4240186fc0bb19b641435a7a484c923
Mitnez <68821845+Mitnez@users.noreply.github.com>
2021-05-14 18:40:40 -0500

    Add files via upload

--- ip.py
+++ ip.py
@@ -1,54 +1,28 @@
-# traducido por m1t (creditos noob-hackers)
-
-# modulos reequeridos
-import argparse
-import requests, json
-import sys
-from sys import argv
-import os
-
-# ni puta idea realmente, m1t del futuro sabe que onda
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument ("-ip", help= "target/host IP address", type=str, dest='target', required=True )
-
-args = parser.parse_args()
-
-#banner of script
-
-print ("         <===[[ coded by N17RO ]]===> \n")
-print ("   <---(( Traducido por m1t xdxd ))--> \n")
-
-
-ip = args.target
-
-api = "http://ip-api.com/json/"
-
-try:
-        data = requests.get(api+ip).json()
-        sys.stdout.flush()
-        print ("[Victima]:", data['query'])
-        print("<--------------->")
-        print ("[Proveedor]:", data['isp'])
-        print("<--------------->")
-        print ("[Organizacion]:", data['org'])
-        print("<--------------->")
-        print ("[Ciudad]:", data['city'])
-        print("<--------------->")
-        print ("[Region]:", data['region'])
-        print("<--------------->")
-        print ("[Longitud]:", data['lon'])
-        print("<--------------->")
-        print ("[Latitiud]:", data['lat'])
-        print("<--------------->")
-        print ("[Zona horaria]:", data['timezone'])
-        print("<--------------->")
-        print ("[Codigo postal]:", data['zip'])
-
-except KeyboardInterrupt:
-        print ('Adios, ten un buen dia :)')
-        sys.exit(0)
-except requests.exceptions.ConnectionError as e:
-        print ("[~]"+" No tengo internet, no sirvo")
-sys.exit(1)
+# importing the requests library
+import requests
+  
+# api-endpoint
+URL = "http://maps.googleapis.com/maps/api/geocode/json"
+  
+# location given here
+location = "delhi technological university"
+  
+# defining a params dict for the parameters to be sent to the API
+PARAMS = {'address':location}
+  
+# sending get request and saving the response as response object
+r = requests.get(url = URL, params = PARAMS)
+  
+# extracting data in json format
+data = r.json()
+  
+  
+# extracting latitude, longitude and formatted address 
+# of the first matching location
+latitude = data['results'][0]['geometry']['location']['lat']
+longitude = data['results'][0]['geometry']['location']['lng']
+formatted_address = data['results'][0]['formatted_address']
+  
+# printing the output
+print("Latitude:%s\nLongitude:%s\nFormatted Address:%s"
+      %(latitude, longitude,formatted_address))
